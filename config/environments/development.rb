f9a4aef0b87fe3462982b362366f6a1438cc1d64
YuriNakano <yuriinlethbridge@outlook.com>
2022-11-21 14:11:58 +0000

    Fix blocked host error

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "7afc8e6d8eec459da3e9d49058891976.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
