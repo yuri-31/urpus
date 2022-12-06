f12c32cbb29d0caef4f0659335a83775ea5245b1
YuriNakano <yuriinlethbridge@outlook.com>
2022-12-06 18:49:17 +0000

    Update public/sessions#new

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -3,9 +3,9 @@
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
   
-  # def after_sign_in_path_for(resource)
-  #   root_path
-  # end
+  def after_sign_in_path_for(resource)
+    root_path
+  end
   
   def after_sign_out_path_for(resource)
     root_path
