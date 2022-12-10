22c147e13b64db4e9cac1e1c07da7ca0354bdd82
YuriNakano <yuriinlethbridge@outlook.com>
2022-12-10 12:10:33 +0000

    Add enum_help gem

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,2 +1,8 @@
 class Word < ApplicationRecord
+    
+    belongs_to :page
+    has_many :meanings
+    has_many :examples
+    
+    enum form: {verb: 0, noun: 1, adjective: 2, adverb: 3, phrase: 4, idiom: 5}
 end
