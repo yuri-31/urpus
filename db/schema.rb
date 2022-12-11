0f83d085f6feeaeac702289b32f6a455051e2934
YuriNakano <yuriinlethbridge@outlook.com>
2022-12-11 13:17:01 +0000

    Add :user_id column to :words table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_11_095730) do
+ActiveRecord::Schema.define(version: 2022_12_11_130400) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -128,8 +128,11 @@ ActiveRecord::Schema.define(version: 2022_12_11_095730) do
     t.boolean "is_learnt", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_words_on_user_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "words", "users"
 end
