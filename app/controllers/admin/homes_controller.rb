f0483b15c7363e0a4c0cd5473b5a94a7adbd0f7a
YuriNakano <yuriinlethbridge@outlook.com>
2023-01-04 16:15:36 +0000

    Make it possible to display columns by topics

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,6 +1,12 @@
 class Admin::HomesController < ApplicationController
     def top
-        @topics = current_admin.topics
-        @columns = Column.all
+        @topics = Topic.all
+        
+        if params[:topic_id]
+            @topic = Topic.find(params[:topic_id])
+            @columns = @topic.columns.reverse_order
+        else
+            @columns = Column.all.reverse_order
+        end
     end
 end
--- app/controllers/public/topics_controller.rb
+++ app/controllers/public/topics_controller.rb
@@ -1,6 +1,12 @@
 class Public::TopicsController < ApplicationController
     def index
         @topics = Topic.all
-        @columns = Column.where(is_public: true)
+        
+        if params[:topic_id]
+            @topic = Topic.find(params[:topic_id])
+            @columns = @topic.columns.where(is_public: true).reverse_order
+        else
+            @columns = Column.where(is_public: true)
+        end
     end
 end
