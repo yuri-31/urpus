229380a8d41ccaa3a168a64b58a04cc023850ab6
YuriNakano <yuriinlethbridge@outlook.com>
2023-01-07 20:37:54 +0000

    Don't allow deleted users sign in

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :user_state, only: [:create]
   
   def after_sign_in_path_for(resource)
     root_path
@@ -9,6 +10,17 @@ class Public::SessionsController < Devise::SessionsController
   
   def after_sign_out_path_for(resource)
     root_path
+  end
+  
+  
+  
+  protected
+  def user_state
+    @user = User.find_by(email: params[:user][:email])
+    return if !@user
+    if @user.valid_password?(params[:user][:password]) && @user.is_deleted == true
+      redirect_to new_user_registration_path
+    end
   end
 
   def guest_sign_in
